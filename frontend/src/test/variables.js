adcddca8fc541d4dd47498f3715e6ea9880be460
BrunoPierca <pierca18@gmail.com>
2024-02-13 12:52:17 -0300

    Create variables.js

---
+++ frontend/src/test/variables.js
@@ -0,0 +1,18 @@
+export const singleFile = {
+    "file": "mockFile.csv",
+    "lines": [
+        { "text": "textMock", "number": 9, "hex": "hexMock" },
+        { "text": "aDmpjyPFzlKozSXPFxbIsgLGsQvpByZF", "number": 9037789, "hex": "b1632b6c872ff8b838c9af2000a32712" },
+        { "text": "kVVGuXSash", "number": 4239437, "hex": "a6cb623b8b88f6892ee5b4438e6ad000" },
+        { "text": "SC", "number": 80069, "hex": "5d285025bf934c0bdf5f032058254f04" },
+        { "text": "fuAJlKTTcwKTfDDMIAjgLdQkc", "number": 135379, "hex": "04b95c1d72558be966924326dce48e8d" },
+        { "text": "NMYgtJAqngrhgbAImx", "number": 56076309, "hex": "3234d2f78bf696b708642e1dab0d3a75" },
+        { "text": "sAYdjTRspsnIBbjUeBBx", "number": 1, "hex": "5bce046cd49fceb90a41f3200c299843" },
+        { "text": "ZbhqNCv", "number": 142365370, "hex": "10ae0b164b787f530c321b915f31662b" },
+        { "text": "tijUnEFOppFWNxSNJR", "number": 84702499, "hex": "01718e549354086a7ee16f420b9fd165" },
+        { "text": "ewzthFwVemVvKppkOBDNPA", "number": 929114141, "hex": "18e3e5665793e64fbb3c68b5583c3204" },
+        { "text": "vnQuoJBRGDMlWTsShBL", "number": 5.183965059276523e+31, "hex": "b7e20ba6e452a44c686d87f71aaab947" },
+        { "text": "fONQKahQNzlkOvn", "number": 42, "hex": "aa638870a41acae8390a1d0715708490" }
+    ]
+}
+
